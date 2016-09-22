d48b1ac86491f2d3f9812fd97b3b09c98c070e91
Mythral <MythralFTW@gmail.com>
2016-09-21 20:18:37 -0500

    Created Inventory System

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickup : MonoBehaviour
+{
+	public string ItemName = "";     // The name of the item to give
+	public int Quantity = 1;	// The quantity of the item to give
+
+	public void OnTriggerEnter(Collider other)
+	{
+		// Attempt to give the player the item, if its successful, destroy the physical object
+		if (other.gameObject.GetComponent<Inventory>().AddItem(ItemName, Quantity))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	public Dictionary<string, int> Items = new Dictionary<string, int>();
+
+	public bool AddItem(string itemName, int amount = 1)
+	{
+		// If the item is found in the players inventory the just increase the quantity of the item
+		if (Items.ContainsKey(itemName))
+		{
+			// TODO: This is where we could add in checking for a maximum quantity or conditions
+			Items[itemName] += amount;
+			return true;
+		}
+
+		// Otherwise create a new Item and it to the dictionary
+		Items.Add(itemName, 1);
+
+		return true;
+	}
+
+	public bool RemoveItem(string itemName)
+	{
+		// Check the players inventory for any items with the same name
+		var item = Items.FirstOrDefault(i => i.Key == itemName);
+
+		// If the item is found in the players inventory then proceed otherwise return false.
+		if (Items.ContainsKey(itemName))
+		{
+			// If we have more than one of the item remove 1 from the players inventory.
+			if (item.Value > 1)
+			{
+				Items[itemName] -= 1;
+				return true;
+			}
+
+			// Otherwise just remove the item from the players inventory.
+			return Items.Remove(itemName);
+		}
+
+		return false;
+	}
+
+	public void DisplayInventory()
+	{
+		Debug.Log("Inventory:");
+		foreach (var i in Items)
+		{
+			Debug.Log(string.Format("{0}: {1}", i.Key, i.Value));
+		}
+	}
+}
